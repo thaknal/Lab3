7180b9d96225d1349921f6d20fe7f6acc4995a1e
thaknal <krzysiekpnq50@gmail.com>
2021-05-10 16:18:25 +0200

    Zadanie 4 C)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class Controller {
+    public Button btnMain;
+
+    public void onBtnAction(ActionEvent actionEvent) {
+    }
 }
