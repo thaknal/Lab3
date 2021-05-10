0a4928c1d751f3ceeca1aab5cd852a65c86458a8
thaknal <krzysiekpnq50@gmail.com>
2021-05-10 16:23:00 +0200

    Zadanie 4 D)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,7 +5,9 @@ import javafx.scene.control.Button;
 
 public class Controller {
     public Button btnMain;
+    int clickCounter = 0;
 
     public void onBtnAction(ActionEvent actionEvent) {
+        System.out.println("Kliknięcie nr " + ++clickCounter);
     }
 }
