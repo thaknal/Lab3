994ff32675b26f67a8a09d6a5c0ff1e5c048e85f
thaknal <krzysiekpnq50@gmail.com>
2021-05-17 12:26:36 +0200

    Zadanie 6

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,6 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setWidth(600);
+        primaryStage.setHeight(600);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
